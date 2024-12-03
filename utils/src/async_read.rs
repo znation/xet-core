4b01406ae451a3ff96b8817c70ce44d6d8108ec8
Assaf Vayner <assaf@huggingface.co>
2024-12-03 13:58:13 -0800

    copy_reader holds a writer (#105)
    * copy_reader writes to writer
    
    * naming changes
    
    * fmty

--- utils/src/async_read.rs
+++ utils/src/async_read.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::pin::Pin;
 use std::task::{ready, Context, Poll};
 
@@ -7,36 +8,36 @@ use futures::AsyncRead;
 // wraps over an AsyncRead, copying all the contents read from the inner reader
 // and buffers it in an internal buffer which can be retrieved by calling .consume()
 // to return a copy of all the content that was read.
-pub struct CopyReader<'a, T> {
-    src: Pin<&'a mut T>,
-    buf: Vec<u8>,
+pub struct CopyReader<'r, 'w, R, W> {
+    src: Pin<&'r mut R>,
+    writer: &'w mut W,
 }
 
-impl<'a, T: AsyncRead + Unpin> AsyncRead for CopyReader<'a, T> {
+impl<'r, 'w, R: AsyncRead + Unpin, W: Write> AsyncRead for CopyReader<'r, 'w, R, W> {
     fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<std::io::Result<usize>> {
         let res = ready!(self.src.as_mut().poll_read(cx, buf))?;
-        self.buf.extend_from_slice(&buf[..res]);
+        self.writer.write_all(&buf[..res])?;
         Poll::Ready(Ok(res))
     }
 }
 
-impl<'a, T: AsyncRead + Unpin> Unpin for CopyReader<'a, T> {}
+impl<'r, 'w, R: AsyncRead + Unpin, W: Write> Unpin for CopyReader<'r, 'w, R, W> {}
 
-impl<'a, T: AsyncRead + Unpin> CopyReader<'a, T> {
-    pub fn new(src: Pin<&'a mut T>) -> Self {
-        Self { src, buf: Vec::new() }
-    }
-
-    pub fn consume(self) -> Vec<u8> {
-        self.buf
+impl<'r, 'w, R: AsyncRead + Unpin, W: Write> CopyReader<'r, 'w, R, W> {
+    pub fn new(src: &'r mut R, writer: &'w mut W) -> Self {
+        let src = Pin::new(src);
+        Self { src, writer }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::{Read, Seek, SeekFrom};
+
     use bytes::Bytes;
     use futures::io::Cursor;
     use futures::{AsyncReadExt, TryStreamExt};
+    use tempfile::tempfile;
 
     use super::*;
 
@@ -51,10 +52,36 @@ mod tests {
         ];
 
         for mut reader in readers {
-            let mut copy_reader = CopyReader::new(Pin::new(&mut reader));
+            let mut writer = Vec::new();
+            let mut copy_reader = CopyReader::new(&mut reader, &mut writer);
             let mut buf = Vec::new();
             assert!(copy_reader.read_to_end(&mut buf).await.is_ok());
-            assert_eq!(buf, copy_reader.consume());
+            assert_eq!(buf, writer);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_copy_reader_to_file() {
+        let readers: Vec<Box<dyn AsyncRead + Unpin>> = vec![
+            Box::new(Cursor::new("abcdeflaksjdlakjsldkajlfkjal".as_bytes())),
+            Box::new(Cursor::new(vec![0x88; 10000])),
+            n_stream(3),
+            n_stream(500),
+            n_stream(10000),
+        ];
+
+        for mut reader in readers {
+            let mut writer = tempfile().unwrap();
+            let mut copy_reader = CopyReader::new(&mut reader, &mut writer);
+            let mut buf = Vec::new();
+            assert!(copy_reader.read_to_end(&mut buf).await.is_ok());
+
+            // read file to compare contents
+            assert!(writer.seek(SeekFrom::Start(0)).is_ok());
+            let mut file_contents = Vec::new();
+            assert!(writer.read_to_end(&mut file_contents).is_ok());
+
+            assert_eq!(buf, file_contents);
         }
     }
 
@@ -63,10 +90,11 @@ mod tests {
         let readers: Vec<Box<dyn AsyncRead + Unpin>> = vec![Box::new(Cursor::new(vec![0x88; 1024])), n_stream(10000)];
 
         for mut reader in readers {
-            let mut copy_reader = CopyReader::new(Pin::new(&mut reader));
+            let mut writer = Vec::new();
+            let mut copy_reader = CopyReader::new(&mut reader, &mut writer);
             let mut buf = vec![0; 512];
             assert!(copy_reader.read_exact(&mut buf).await.is_ok());
-            assert_eq!(buf, copy_reader.consume());
+            assert_eq!(buf, writer);
         }
     }
 
