1018c822798f9c0d29e51a957b54b13d3d00cee7
Di Xiao <di@huggingface.co>
2024-12-12 04:46:11 +0800

    Each PFT uses isolated shard session directory (#113)
    * Each PFT uses isolated shard session directory
    
    * fix linting

--- data/src/configurations.rs
+++ data/src/configurations.rs
@@ -1,15 +1,11 @@
-use std::env::current_dir;
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::sync::Arc;
 
 use cas_client::CacheConfig;
-use dirs::home_dir;
-use utils::auth::{AuthConfig, TokenRefresher};
+use utils::auth::AuthConfig;
 
 use crate::errors::Result;
 use crate::repo_salt::RepoSalt;
-use crate::{errors, SMALL_FILE_THRESHOLD};
 
 #[derive(Debug)]
 pub enum Endpoint {
@@ -125,55 +121,4 @@ impl TranslatorConfig {
 
         Ok(())
     }
-}
-
-pub fn default_config(
-    endpoint: String,
-    token_info: Option<(String, u64)>,
-    token_refresher: Option<Arc<dyn TokenRefresher>>,
-) -> errors::Result<TranslatorConfig> {
-    let home = home_dir().unwrap_or(current_dir()?);
-    let xet_path = home.join(".xet");
-    std::fs::create_dir_all(&xet_path)?;
-
-    let cache_path = home.join(".cache").join("huggingface").join("xet");
-
-    let (token, token_expiration) = token_info.unzip();
-    let auth_cfg = AuthConfig::maybe_new(token, token_expiration, token_refresher);
-
-    let translator_config = TranslatorConfig {
-        file_query_policy: FileQueryPolicy::ServerOnly,
-        cas_storage_config: StorageConfig {
-            endpoint: Endpoint::Server(endpoint.clone()),
-            auth: auth_cfg.clone(),
-            prefix: "default".into(),
-            cache_config: Some(CacheConfig {
-                cache_directory: cache_path.join("chunk-cache"),
-                cache_size: 10 * 1024 * 1024 * 1024, // 10 GiB
-            }),
-            staging_directory: None,
-        },
-        shard_storage_config: StorageConfig {
-            endpoint: Endpoint::Server(endpoint),
-            auth: auth_cfg,
-            prefix: "default-merkledb".into(),
-            cache_config: Some(CacheConfig {
-                cache_directory: cache_path.join("shard-cache"),
-                cache_size: 0, // ignored
-            }),
-            staging_directory: Some(xet_path.join("shard-session")),
-        },
-        dedup_config: Some(DedupConfig {
-            repo_salt: None,
-            small_file_threshold: SMALL_FILE_THRESHOLD,
-            global_dedup_policy: Default::default(),
-        }),
-        repo_info: Some(RepoInfo {
-            repo_paths: vec!["".into()],
-        }),
-    };
-
-    translator_config.validate()?;
-
-    Ok(translator_config)
 }
--- data/src/data_client.rs
+++ data/src/data_client.rs
@@ -1,24 +1,85 @@
+use std::env::current_dir;
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::path::PathBuf;
 use std::sync::Arc;
 
+use cas_client::CacheConfig;
+use dirs::home_dir;
 use parutils::{tokio_par_for_each, ParallelError};
-use utils::auth::TokenRefresher;
+use tempfile::{tempdir_in, TempDir};
+use utils::auth::{AuthConfig, TokenRefresher};
 use utils::progress::ProgressUpdater;
 use utils::ThreadPool;
 
-use crate::configurations::default_config;
+use crate::configurations::*;
 use crate::errors::DataProcessingError;
 use crate::{errors, PointerFile, PointerFileTranslator};
 
-/// The maximum git filter protocol packet size
+// Concurrency in number of files
 pub const MAX_CONCURRENT_UPLOADS: usize = 8; // TODO
 pub const MAX_CONCURRENT_DOWNLOADS: usize = 8; // TODO
 
+// We now process every file delegated from the Python library.
+const SMALL_FILE_THRESHOLD: usize = 1;
+
 const DEFAULT_CAS_ENDPOINT: &str = "http://localhost:8080";
 const READ_BLOCK_SIZE: usize = 1024 * 1024;
+
+pub fn default_config(
+    endpoint: String,
+    token_info: Option<(String, u64)>,
+    token_refresher: Option<Arc<dyn TokenRefresher>>,
+) -> errors::Result<(TranslatorConfig, TempDir)> {
+    let home = home_dir().unwrap_or(current_dir()?);
+    let xet_path = home.join(".xet");
+    std::fs::create_dir_all(&xet_path)?;
+
+    let cache_path = home.join(".cache").join("huggingface").join("xet");
+
+    let (token, token_expiration) = token_info.unzip();
+    let auth_cfg = AuthConfig::maybe_new(token, token_expiration, token_refresher);
+
+    let shard_staging_directory = tempdir_in(xet_path.join("shard-session"))?;
+
+    let translator_config = TranslatorConfig {
+        file_query_policy: FileQueryPolicy::ServerOnly,
+        cas_storage_config: StorageConfig {
+            endpoint: Endpoint::Server(endpoint.clone()),
+            auth: auth_cfg.clone(),
+            prefix: "default".into(),
+            cache_config: Some(CacheConfig {
+                cache_directory: cache_path.join("chunk-cache"),
+                cache_size: 10 * 1024 * 1024 * 1024, // 10 GiB
+            }),
+            staging_directory: None,
+        },
+        shard_storage_config: StorageConfig {
+            endpoint: Endpoint::Server(endpoint),
+            auth: auth_cfg,
+            prefix: "default-merkledb".into(),
+            cache_config: Some(CacheConfig {
+                cache_directory: cache_path.join("shard-cache"),
+                cache_size: 0, // ignored
+            }),
+            staging_directory: Some(shard_staging_directory.path().to_owned()),
+        },
+        dedup_config: Some(DedupConfig {
+            repo_salt: None,
+            small_file_threshold: SMALL_FILE_THRESHOLD,
+            global_dedup_policy: Default::default(),
+        }),
+        repo_info: Some(RepoInfo {
+            repo_paths: vec!["".into()],
+        }),
+    };
+
+    translator_config.validate()?;
+
+    // Return the temp dir so that it's not dropped and thus the directory deleted.
+    Ok((translator_config, shard_staging_directory))
+}
 
 pub async fn upload_async(
     threadpool: Arc<ThreadPool>,
@@ -31,7 +92,8 @@ pub async fn upload_async(
     // produce Xorbs + Shards
     // upload shards and xorbs
     // for each file, return the filehash
-    let config = default_config(endpoint.unwrap_or(DEFAULT_CAS_ENDPOINT.to_string()), token_info, token_refresher)?;
+    let (config, _tempdir) =
+        default_config(endpoint.unwrap_or(DEFAULT_CAS_ENDPOINT.to_string()), token_info, token_refresher)?;
 
     let processor = Arc::new(PointerFileTranslator::new(config, threadpool).await?);
     // for all files, clean them, producing pointer files.
@@ -66,7 +128,8 @@ pub async fn download_async(
             ));
         }
     }
-    let config = default_config(endpoint.unwrap_or(DEFAULT_CAS_ENDPOINT.to_string()), token_info, token_refresher)?;
+    let (config, _tempdir) =
+        default_config(endpoint.unwrap_or(DEFAULT_CAS_ENDPOINT.to_string()), token_info, token_refresher)?;
 
     let updaters = match progress_updaters {
         None => vec![None; pointer_files.len()],
