be0a5b337083d461862f753785da4009c61dba1d
Assaf Vayner <assaf@huggingface.co>
2025-02-19 13:55:59 -0800

    only store compressed chunk if the compressed chunk is smaller (#177)
    Changes the chunk serialization functionality to only use the
    compression scheme if it is actually better than storing the data raw.
    In the case where the uncompressed data takes less space, the chunk is
    serialized with no compression.

--- cas_object/src/cas_chunk_format.rs
+++ cas_object/src/cas_chunk_format.rs
@@ -112,16 +112,20 @@ pub fn serialize_chunk<W: Write>(
     w: &mut W,
     compression_scheme: CompressionScheme,
 ) -> Result<usize, CasObjectError> {
-    let uncompressed_len = chunk.len();
-
     let compressed = compression_scheme.compress_from_slice(chunk)?;
-    let compressed_len = compressed.len();
-    let header = CASChunkHeader::new(compression_scheme, compressed_len as u32, uncompressed_len as u32);
 
+    // set compression scheme and compressed data buffer to no compression if the compressed
+    // length is longer than uncompressed
+    let (compression_scheme, compressed) = if compressed.len() >= chunk.len() {
+        (CompressionScheme::None, chunk.into())
+    } else {
+        (compression_scheme, compressed)
+    };
+    let header = CASChunkHeader::new(compression_scheme, compressed.len() as u32, chunk.len() as u32);
     write_chunk_header(w, &header)?;
     w.write_all(&compressed)?;
 
-    Ok(size_of::<CASChunkHeader>() + compressed_len)
+    Ok(size_of::<CASChunkHeader>() + compressed.len())
 }
 
 pub fn parse_chunk_header(chunk_header_bytes: [u8; CAS_CHUNK_HEADER_LENGTH]) -> Result<CASChunkHeader, CasObjectError> {
